78150be9f7cf93d1a78c7026df354b5c6bfb7b32
Xinyu Tian <sarahtxy@gmail.com>
2016-03-12 20:48:37 -0500

    mixed effect

--- simulation_linear.R
+++ simulation_linear.R
@@ -1,16 +1,27 @@
-simulation <- function(N, P, aP, a, b, c, d, eff.size = 1, seed = NULL) {
+simulation <- function(N, P, aP, a, b, c, d, eff.size = 1, eff = "random", seed = NULL) {
   if(! is.null(seed)) set.seed(seed)
   X <- matrix(nrow = N, ncol = P)
   y <- matrix(nrow = N, ncol = 1)
-  for (i in 1:N) {
+  if (eff == "random") {
+    for (i in 1:N) {
+      alpha = rgamma(1, a, b)
+      lambda = rgamma(1, c, d)
+      beta = rnorm(P, 0, sd = sqrt(1 / alpha))
+      beta[1:aP] = beta[1:aP] + eff.size
+      
+      X[i, ] = rnorm(P)
+      
+      y[i, ] = rnorm(1, t(beta) %*% X[i, ], sqrt(1 / lambda))
+    }
+  } else if (eff == "fixed") {
     alpha = rgamma(1, a, b)
     lambda = rgamma(1, c, d)
     beta = rnorm(P, 0, sd = sqrt(1 / alpha))
     beta[1:aP] = beta[1:aP] + eff.size
-    
-    X[i, ] = rnorm(P)
-    
-    y[i, ] = rnorm(1, t(beta) %*% X[i, ], sqrt(1 / lambda))
+    for (i in 1:N) {
+      X[i, ] = rnorm(P)
+    }
+    y <- X %*% beta + rnorm(N, 0, sqrt(1 / lambda))
   }
   return (list("x" = X, "y" = y))
 }
